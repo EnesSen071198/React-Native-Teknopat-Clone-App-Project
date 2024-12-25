d4d5e9cc5a85730b8d16be97cff00f0bbdab0fe3
Enes Şen <enessen071198@gmail.com>
2024-12-25 22:10:18 +0300

    Header Componenti tamamlandı, Kategorilerin Componentleri oluşturuldu,Drawer eklendi,HomeScreen componentine import edildi

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import HomeScreen from "./src/screens/HomeScreen";
+import CustomDrawerContent from "./src/components/Drawer";
+import Header from "./src/components/Header";
+import Category1 from "./src/pages/Category1";
+import YapayZeka from "./src/pages/YapayZeka";
+import Haber from "./src/pages/Haber";
+import Makale from "./src/pages/Makale";
+import Video from "./src/pages/Video";
+import Tavsiyeler from "./src/pages/Tavsiyeler";
+import Sosyal from "./src/pages/Sosyal";
+import Etkinlik from "./src/pages/Etkinlik";
 
-export default function App() {
+const Drawer = createDrawerNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName='Home'
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+        screenOptions={{
+          header: () => <Header />
+        }}>
+        <Drawer.Screen name='Ana Sayfa' component={HomeScreen} />
+        <Drawer.Screen name='Category1' component={Category1} />
+        <Drawer.Screen name='Yapay Zeka' component={YapayZeka} />
+        <Drawer.Screen name='Haber' component={Haber} />
+        <Drawer.Screen name='Makale' component={Makale} />
+        <Drawer.Screen name='Video' component={Video} />
+        <Drawer.Screen name='Tavsiyeler' component={Tavsiyeler} />
+        <Drawer.Screen name='Sosyal' component={Sosyal} />
+        <Drawer.Screen name='Etkinlik' component={Etkinlik} />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        {/* Diğer kategoriler için ekranları ekleyin */}
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
