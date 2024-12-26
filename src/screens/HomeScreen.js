62a37b0dfea1319a87e837d31a53b80126e99a25
Enes Şen <enessen071198@gmail.com>
2024-12-26 09:47:29 +0300

    HomeScreen düzenlendi ve son haline getirildi

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { View, Text, Image, FlatList, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  ImageBackground,
+  FlatList,
+  StyleSheet
+} from "react-native";
 import data from "../data/Data.json"; // Dosyayı içe aktar
 
 const HomeScreen = () => {
@@ -10,10 +16,17 @@ const HomeScreen = () => {
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item }) => (
           <View style={styles.post}>
-            <Image source={{ uri: item.image }} style={styles.image} />
-            <Text style={styles.title}>{item.title}</Text>
-            <Text style={styles.description}>{item.description}</Text>
-            <Text style={styles.category}>{item.category}</Text>
+            <ImageBackground
+              source={{ uri: item.image }}
+              style={styles.image}
+              imageStyle={{ borderRadius: 10 }}>
+              <View style={styles.overlay} />
+              <View style={styles.textContainer}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.description}>{item.description}</Text>
+                <Text style={styles.readTime}>{item.readTime}</Text>
+              </View>
+            </ImageBackground>
           </View>
         )}
         scrollEnabled={true}
@@ -34,21 +47,37 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: 200,
+    borderRadius: 10,
+    justifyContent: "flex-end"
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0,0,0,0.5)",
     borderRadius: 10
+  },
+  textContainer: {
+    padding: 10,
+    position: "absolute",
+    bottom: 0,
+    left: 0
   },
   title: {
     fontSize: 18,
     fontWeight: "bold",
-    marginVertical: 5
+    color: "white",
+    marginBottom: 5
   },
   description: {
     fontSize: 14,
-    color: "gray"
+    color: "white",
+    marginBottom: 25
   },
-  category: {
+  readTime: {
     fontSize: 12,
-    color: "blue",
-    marginTop: 5
+    color: "white",
+    position: "absolute",
+    bottom: 10,
+    left: 10
   }
 });
 
